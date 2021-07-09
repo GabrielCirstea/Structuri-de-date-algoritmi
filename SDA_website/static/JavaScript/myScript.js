1a75180fc55025739e12dbd327bd83d2c805da76
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2021-07-09 23:01:45 +0300

    remove flask code

--- SDA_website/static/JavaScript/myScript.js
+++
@@ -1,9 +0,0 @@
-function afisare(id) {
-	// face elementele de height auto -- pt sertarase
-  var x = document.getElementById(id);
-  if (x.style.height === "100px") {
-    x.style.height = "auto";
-  } else {
-    x.style.height	= "100px";
-  }
-} 
